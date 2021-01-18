d9dc68c7611ad8387e42df3ffb096bed88c4e491
AlejandroDiaz <alejandro.hernandez@bootcamp-upgrade.com>
2021-01-18 13:36:38 +0100

    dropdown menu styled

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Hero from './components/Hero';
 import { Navbar } from './components/Navbar';
 import GlobalStyle from './globalStyles';
 import {SliderData} from './data/SliderData';
+import Dropdown from './components/Dropdown';
 
 function App() {
   return (
     <>
     <GlobalStyle />
       <Navbar />
+      <Dropdown />
       <Hero slides={SliderData} />
     </>
   );
