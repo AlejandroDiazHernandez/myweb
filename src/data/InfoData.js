1c01085375ce98e36f127e5dc812702f2ad85ec7
AlejandroDiaz <alejandro.hernandez@bootcamp-upgrade.com>
2021-01-19 17:09:07 +0100

    info section

---
+++ src/data/InfoData.js
@@ -0,0 +1,20 @@
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Random things Random things Random things Random things Random things Random things',
+    paragraphTwo: 'Another Random things Random things Random things Random things Random things Random things',
+    buttonLabel: 'View Homes',
+    image: process.env.PUBLIC_URL + '/images/house5.jpg',
+    reverse: false,
+    delay: 100,
+}
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Random things Random things Random things Random things Random things Random thingsRandom thingsRandom thingsRandom thingsRandom thingsRandom thingsRandom things',
+    paragraphTwo: 'Another Random things Random things Random things',
+    buttonLabel: 'View Homes',
+    image: process.env.PUBLIC_URL + '/images/house6.jpg',
+    reverse: true,
+    delay: 300,
+}
